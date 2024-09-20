c356534d31430a1b22f5a4c132959c953a5d8577
Vitaly Grinberg <vgrinber@redhat.com>
2024-09-20 16:17:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# Visualize function call duration
+
+This repo contains an example of function call duration statistics collected using ftrace and visualized with pyplot.
+
+## The use case ##
+
+PTP grand-leader realized with three Intel E810-XXVDA4T cards. One of the cards has GNSS receiver. Two other cards are synced to the first by 1PPS link. The `ts2phc` program is synchronizing the PHC counters on all the three cards to the nmea stream from the 1st card.
+Error of one second in PHC timestamp occured from time to time, leading to the suspicion that PHC update might stuck in the driver. Needed to analyze system call duration statistically.
+
+## ftrace configuration ##
+
+```bash
+echo 'function_graph' > current_tracer
+echo pc_clock_adjtime >set_graph_function
+echo *adjtime*  *ptp* > set_ftrace_filter 
+echo funcgraph-abstime > trace_options
+```
+
+## Trace example ##
+The trace sample is in [sample-trace](./sample-trace)
+
+## Install ##
+```bash
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+## Run ##
+```bash
+python3 parse.py sample-trace ice_ptp_adjfine
+```
+## Results ##
+Peaks of 100ms discovered in a 20-minutes run.
+
+![Figure_1.png](./Figure_1.png)
+
+![Figure_2.png](./Figure_2.png)
---
+++ parse.py
@@ -0,0 +1,57 @@
+import sys
+import json
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def parse_file (fn, func):
+    durations = []
+    with open(fn) as f:
+        lines = f.readlines()
+    state = -1
+    for line in lines:
+        if func in line and "/*" not in line:
+            state = 0
+        if state >= 0:
+            if "{" in line:
+                state += 1
+            if "}" in line:
+                state -= 1
+            if state == 0:
+                ts = float(line.split("|")[0].strip(" "))
+                duration = float(line.split("|")[-2].split("us")[-2].strip(" ").split(" ")[-1])
+                state = -1
+                durations.append({ts: duration})
+    return durations
+
+
+def plot(durations, func):
+    _, ax = plt.subplots()
+    ys = list([list(sample.items())[0][-1] for sample in durations])
+    ax.set_xlabel("Sample")
+    ax.set_ylabel("Call duration, usec")
+    ax.set_title(f"{func} execution time")
+    ax.plot(np.array(ys))
+    _, ax2 = plt.subplots()
+    ax2.hist(ys, bins=100, color="Gray")
+    ax2.set_yscale('log')
+    ax2.set_title(f"{func} duration histogram (logY) and CDF")
+    count, _ = np.histogram(ys, bins=100)
+    pdf = count / sum(count)
+    cdf = np.cumsum(pdf)
+    ax3 = ax2.twinx()
+    ax3.plot(np.arange(0, max(ys), max(ys)/100), cdf, label='CDF')
+    plt.show()
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: python parse.py <trace file name> <function name>, for example:")
+        print("python3 parse.py trace_pipe-3 ice_ptp_adjfine")
+        sys.exit(0)
+    file_name = sys.argv[1]
+    func_name = sys.argv[2]
+    durations = parse_file(file_name, func_name)
+    # Uncomment to dump data to json
+    # with open(f"{file_name}_{func_name}.json", "w") as fw:
+    #     json.dump(durations, fw)
+    plot(durations, func_name)
