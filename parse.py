3cd02c9bbb6bd8eb6e9954ca79979b1ed37e7938
Vitaly Grinberg <vgrinber@redhat.com>
2024-09-21 11:34:45 +0300

    Add peak and annotations

--- parse.py
+++ parse.py
@@ -27,16 +27,37 @@ def parse_file (fn, func):
 
 def plot(durations, func):
     _, ax = plt.subplots()
-    ys = list([list(sample.items())[0][-1] for sample in durations])
+    ys = list([list(sample.items())[0][-1]/1000 for sample in durations])
+    max_y = max(ys)
     ax.set_xlabel("Sample")
-    ax.set_ylabel("Call duration, usec")
+    ax.set_ylabel("Call duration, ms")
     ax.set_title(f"{func} execution time and average")
     ax.plot(np.array(ys))
+    ax.set_ybound((0, max_y*1.2))
+    idx_max_y = ys.index(max_y)
+
+    peak = ax.twinx()
+    peak.plot([idx_max_y], [max_y], "brown", marker=".", markersize=20)
+    peak.set_ybound(0, max_y*1.2)
+    peak_position_pt = idx_max_y / len(ys)
+    if peak_position_pt > 0.7:
+        ha = "right"
+    elif peak_position_pt < 0.3:
+        ha = "left"
+    else:
+        ha = "center"
+    timestamp = list(durations[idx_max_y].keys())[0]
+    value = durations[idx_max_y].get(timestamp)
+    peak.annotate(f'peak {value/1000}ms\ntimestamp {timestamp}', 
+                  xy=(idx_max_y, max_y), xytext=(idx_max_y, max_y*1.1),
+                  horizontalalignment=ha, verticalalignment='center',
+                  color='brown')
+
     avg = ax.twinx()
     avg_data = [sum(ys)/len(ys)] * len(ys)
     avg.plot(avg_data, 'r', label='AVG')
-    avg.set_ybound(0, max(ys))
-    
+    avg.set_ybound(0, max_y*1.2)
+
     _, ax2 = plt.subplots()
     ax2.hist(ys, bins=100, color="Gray")
     ax2.set_yscale('log')
